692735e41ab23c38322625187e7b2c60efaa32a0
Mohammed Hossam <mohammedhossam5000@gmail.com>
2024-02-13 15:17:48 -0800

    fixed

--- 0x01-challenge/square.py
+++ 0x01-challenge/square.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python3
+"""module tha contains class"""
+
 
 class square():
-    
+    """class that contains calculations"""
     width = 0
     height = 0
 
-    
     def __init__(self, *args, **kwargs):
         for key, value in kwargs.items():
             setattr(self, key, value)
@@ -15,10 +16,12 @@ class square():
         return self.width * self.width
 
     def PermiterOfMySquare(self):
+        """claculatae the perimeter"""
         return (self.width * 2) + (self.height * 2)
 
     def __str__(self):
         return "{}/{}".format(self.width, self.height)
+
 
 if __name__ == "__main__":
 
